32b4fabf933d457b4af785903063005118f22ab9
Wendson <wendsonsantanajps@gmail.com>
2021-09-07 18:37:49 -0300

    Adicionado comentários

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+// Pegando elemento mobile button
 const mobileButton = document.getElementById("mobilebtn");
 
+// Função toggle mobile button
 function menuToggle(event) {
+  // Se o tipo de evento for touchstart não faça mais nada além do touchstart
   if (event.type === "touchstart") event.preventDefault();
-
+  // Pegando o elemento nav
   const nav = document.getElementById("nav");
+  // Fazendo o elemento nav ter a class active atráves do on/off do toggle
   nav.classList.toggle("active");
+  // Acessibilidade, não sei fazer ainda apenas segui o tutorial
   const active = nav.classList.contains("active");
   event.currentTarget.setAttribute("aria-expanded", active);
   if (active) {
@@ -13,6 +18,7 @@ function menuToggle(event) {
     event.currentTarget.setAttribute("aria-label", "Abrir menu");
   }
 }
-
+// Adicionando evento onclick e usando a função toggle
 mobileButton.addEventListener("click", menuToggle);
+// Adicionando evento touchstart e usando a função toggle
 mobileButton.addEventListener("touchstart", menuToggle);
